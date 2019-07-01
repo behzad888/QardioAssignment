30a84d06a0d76d7fe99f52d60b9cf278fbe5253a
behzad888 <behzad88.2012@gmail.com>
2019-07-01 21:31:07 +0430

    add card tests

--- src/components/__tests__/button-test.js
+++ src/components/__tests__/button-test.js
@@ -1,3 +1,4 @@
+//@flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
--- src/components/__tests__/card-test.js
+++ src/components/__tests__/card-test.js
@@ -25,12 +25,58 @@ describe('while card test running in a browser environment', () => {
       .create(
         <div>
           <Card> This is a Card View </Card>
-          <Card className="card-test"> This is a Card View </Card>
-          <Card className="card-test">
-            <div>This is a Cord View with Card body</div>
+        </div>,
+      )
+      .toJSON();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render header', () => {
+    wrapper = renderer
+      .create(
+        <div>
+          <Card header={<span>Header</span>}> This is a Card View </Card>
+        </div>,
+      )
+      .toJSON();
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should render body', () => {
+    wrapper = renderer
+      .create(
+        <div>
+          <Card>
+            <div>This is Body</div>
           </Card>
-          <Card className="card-test" header={<span>Header</span>}>
-            <div>This is a Cord View with Card body</div>
+        </div>,
+      )
+      .toJSON();
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should render with custom className', () => {
+    wrapper = renderer
+      .create(
+        <div>
+          <Card className="card-test-class-name">
+            <div>This is Body</div>
+          </Card>
+        </div>,
+      )
+      .toJSON();
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should render nested card', () => {
+    wrapper = renderer
+      .create(
+        <div>
+          <Card className="card-test-parent">
+            <div>
+              <Card className="card-test-child">
+                <Card className="card-test-inner-child">
+                  <div>This is Body</div>
+                </Card>
+              </Card>
+            </div>
           </Card>
         </div>,
       )
